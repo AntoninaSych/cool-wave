388df11fb016e2b050618b4b24297627b9c5681a
antonina <atoninasych@gmail.com>
2024-01-06 17:53:12 +0000

    seeders

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $this->call([
+            AdminUserSeeder::class,
+            CountrySeeder::class
+        ]);
+
+        // Do not allow seeders ('test data') to run in a production environment
+        if (app()->environment() !== 'production') {
+            Schema::disableForeignKeyConstraints();
+            Schema::enableForeignKeyConstraints();
+        }
+    }
+}
