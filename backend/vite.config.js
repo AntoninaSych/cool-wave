286718c021c53bc408854fa66b5fd5af65272bc1
antonina <atoninasych@gmail.com>
2024-01-06 18:40:58 +0000

    updated api->APi

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -3,12 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: '192.168.56.10',
-    // port: '5175',
-    watch: {
-      usePolling: true,
-    },
-  },
+  // server: {
+  //   host: '192.168.56.10',
+  //   // port: '5175',
+  //   watch: {
+  //     usePolling: true,
+  //   },
+  // },
   plugins: [vue()]
 })
--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,14 @@
 
 use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
-use  \App\Http\Controllers\api\OrderController;
+use  \App\Http\Controllers\Api\OrderController;
 use \App\Http\Controllers\Api\AuthController;
-use  \App\Http\Controllers\api\UserController;
+use  \App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\CustomerController;
-use App\Http\Controllers\api\DashboardController;
+use App\Http\Controllers\Api\DashboardController;
 use \App\Http\Controllers\ReportController;
-use \App\Http\Controllers\api\CategoryController;
-use App\Http\Controllers\api\PageController;
+use \App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\PageController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
